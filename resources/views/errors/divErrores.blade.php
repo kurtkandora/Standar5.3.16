fe06b31e604bd99ee226c8f0e754045d7957f217
Kurt Kandora <kurt@kandora.cl>
2017-02-28 17:02:58 -0300

    corregido error en la vista de errores

--- resources/views/errors/divErrores.blade.php
+++ resources/views/errors/divErrores.blade.php
@@ -1,9 +1,11 @@
-<div class="alert alert-danger">
-    <ul>
-        @foreach($errors->all() as $error)
-            <li>
-                {{$error}}
-            </li>
-        @endforeach
-    </ul>
-</div>
+@if($errors->all())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach($errors->all() as $error)
+                <li>
+                    {{$error}}
+                </li>
+            @endforeach
+        </ul>
+    </div>
+@endif
